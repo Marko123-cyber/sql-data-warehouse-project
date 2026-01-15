1c74c0a512809b1d02adbae0c37bb41218febb7e
Marko123-cyber <markuspavlovic98@gmail.com>
2026-01-16 00:33:54 +0100

    Create initial_table_exploration.sql

---
+++ scripts/initial_table_exploration.sql
@@ -0,0 +1,27 @@
+select top(1000)
+    *
+from  DataWarehouse.bronze.crm_cust_info;
+
+select top(1000)
+    *
+from bronze.crm_prd_info;
+
+select top(1000)
+    *
+from bronze.crm_sales_details;
+
+select top(1000)
+    *
+from bronze.erp_cust_az12;
+
+
+select top(1000)
+    *
+from bronze.erp_loc_a101;
+
+
+select top(1000)
+    *
+from bronze.erp_px_cat_g1v2;
+
+
