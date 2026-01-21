6ee787dfb95463b5ee194abdad3b015d89c1a455
Marko123-cyber <markuspavlovic98@gmail.com>
2026-01-21 23:29:22 +0100

    Update bronze_ddl_script.sql

--- scripts/bronze/bronze_ddl_script.sql
+++ scripts/bronze/bronze_ddl_script.sql
@@ -40,22 +40,23 @@ CREATE TABLE bronze.crm_prd_info (
 ); 
 go 
 
-IF OBJECT_ID('bronze.crm_sales_orders', 'U') IS NOT NULL
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
     BEGIN 
-        DROP TABLE bronze.crm_sales_orders;
+        DROP TABLE bronze.crm_sales_details ;
     END;
 GO
-CREATE TABLE bronze.crm_sales_orders (
-    sls_ord_num NVARCHAR(50), 
-    sls_prd_key NVARCHAR(50), 
-    sls_cust_id INT, 
-    sls_order_dt INT, 
-    sls_ship_dt INT, 
-    sls_sales INT, 
-    sls_quantity INT, 
-    sls_price INT 
-); 
-go 
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num    NVARCHAR(50),
+    sls_prd_key    NVARCHAR(50),
+    sls_cust_id    INT,
+    sls_order_dt   INT,
+    sls_ship_dt    INT,
+    sls_due_dt     INT,
+    sls_sales      INT,
+    sls_quantity   INT,
+    sls_price      INT 
+);
+go
 
 
 IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
