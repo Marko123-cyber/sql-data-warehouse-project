2657f4d4b179627fa69ab82e614a148691db9aaf
Marko123-cyber <markuspavlovic98@gmail.com>
2026-03-06 18:14:41 +0100

    Update CSV source paths in data loading script

--- scripts/bronze/data_loading_bronze.sql
+++ scripts/bronze/data_loading_bronze.sql
@@ -27,7 +27,7 @@ BEGIN
         TRUNCATE TABLE bronze.crm_cust_info;
         PRINT 'Inserting data into: bronze.crm_cust_info';
         BULK INSERT bronze.crm_cust_info
-        FROM '/var/opt/mssql/data/dwh_project/datasets/source_crm/cust_info.csv'
+        FROM '/var/opt/mssql/data/dwh_project/datasets/crm_source/cust_info.csv'
         WITH (
             FIRSTROW=2,
             FIELDTERMINATOR = ',', 
@@ -49,7 +49,7 @@ BEGIN
         TRUNCATE TABLE bronze.crm_prd_info;
         PRINT 'Inserting data into: bronze.crm_prd_info';
         BULK INSERT bronze.crm_prd_info
-        FROM '/var/opt/mssql/data/dwh_project/datasets/source_crm/prd_info.csv'
+        FROM '/var/opt/mssql/data/dwh_project/datasets/crm_source/prd_info.csv'
         WITH (
             FIRSTROW=2,
             FIELDTERMINATOR = ',', 
@@ -71,7 +71,7 @@ BEGIN
         TRUNCATE TABLE bronze.crm_sales_details;
         PRINT 'Inserting data into: bronze.crm_sales_details';
         BULK INSERT bronze.crm_sales_details
-        FROM '/var/opt/mssql/data/dwh_project/datasets/source_crm/sales_details.csv'
+        FROM '/var/opt/mssql/data/dwh_project/datasets/crm_source/sales_details.csv'
         WITH (
             FIRSTROW=2,
             FIELDTERMINATOR = ',', 
@@ -93,7 +93,7 @@ BEGIN
         TRUNCATE TABLE bronze.erp_cust_az12;
         PRINT 'Inserting data into: bronze.erp_cust_az12';
         BULK INSERT bronze.erp_cust_az12
-        FROM '/var/opt/mssql/data/dwh_project/datasets/source_erp/CUST_AZ12.csv'
+        FROM '/var/opt/mssql/data/dwh_project/datasets/erp_source/CUST_AZ12.csv'
         WITH (
             FIRSTROW=2,
             FIELDTERMINATOR = ',', 
@@ -115,7 +115,7 @@ BEGIN
         TRUNCATE TABLE bronze.erp_loc_a101;
         PRINT 'Inserting data into: bronze.erp_loc_a101';
         BULK INSERT bronze.erp_loc_a101
-        FROM '/var/opt/mssql/data/dwh_project/datasets/source_erp/LOC_A101.csv'
+        FROM '/var/opt/mssql/data/dwh_project/datasets/erp_source/LOC_A101.csv'
         WITH (
             FIRSTROW=2,
             FIELDTERMINATOR = ',', 
@@ -137,7 +137,7 @@ BEGIN
         TRUNCATE TABLE bronze.erp_px_cat_g1v2;
         PRINT 'Inserting data into: bronze.erp_px_cat_g1v2';
         BULK INSERT bronze.erp_px_cat_g1v2
-        FROM '/var/opt/mssql/data/dwh_project/datasets/source_erp/PX_CAT_G1V2.csv'
+        FROM '/var/opt/mssql/data/dwh_project/datasets/erp_source/PX_CAT_G1V2.csv'
         WITH (
             FIRSTROW=2,
             FIELDTERMINATOR = ',', 
